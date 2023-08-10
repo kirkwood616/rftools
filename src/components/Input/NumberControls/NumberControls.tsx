6168feac110ab937b7f63a2aa3adee2c27f194b8
kirkwood616 <kirkwood616@gmail.com>
2023-08-09 21:57:02 -0400

    NumberControls updates

--- src/components/Input/NumberControls/NumberControls.tsx
+++ src/components/Input/NumberControls/NumberControls.tsx
@@ -2,15 +2,17 @@ import { AddSubtract } from "components/Buttons";
 import styles from "./NumberControls.module.css";
 
 interface NumberControlsProps {
+  handleAdd: () => void;
+  handleSubtract: () => void;
   children: React.ReactNode;
 }
 
-export function NumberControls({ children }: NumberControlsProps) {
+export function NumberControls({ handleAdd, handleSubtract, children }: NumberControlsProps) {
   return (
     <div className={styles.NumberControls}>
-      <AddSubtract type="subtract" onClick={() => console.log("subtract")} />
+      <AddSubtract type="subtract" onClick={handleSubtract} />
       {children}
-      <AddSubtract type="add" onClick={() => console.log("add")} />
+      <AddSubtract type="add" onClick={handleAdd} />
     </div>
   );
 }
--- src/components/Input/WholeNumber/WholeNumber.tsx
+++ src/components/Input/WholeNumber/WholeNumber.tsx
@@ -1,21 +1,53 @@
-import { ChangeEvent } from "react";
+import { useState } from "react";
+import { NumberControls } from "..";
 import styles from "../Input.module.css";
 
 interface WholeNumberProps {
   label: string;
   name: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  value: string | number;
+  withControls?: boolean;
 }
 
-export function WholeNumber({ label, name, onChange, value }: WholeNumberProps) {
+export function WholeNumber({ label, name, withControls }: WholeNumberProps) {
+  const [value, setValue] = useState<number>(0);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue((prev) => (Number(e.target.validity.valid) ? Number(e.target.value) : prev));
+  }
+
+  function handleAdd() {
+    setValue((prev) => prev + 1);
+  }
+
+  function handleSubtract() {
+    if (!value) return;
+    setValue((prev) => prev - 1);
+  }
+
+  if (withControls) {
+    return (
+      <NumberControls handleAdd={handleAdd} handleSubtract={handleSubtract}>
+        <input
+          className={styles.input_element}
+          id={label}
+          inputMode="numeric"
+          name={name}
+          onChange={handleChange}
+          pattern="[0-9]*"
+          value={value}
+          type="text"
+        />
+      </NumberControls>
+    );
+  }
+
   return (
     <input
       className={styles.input_element}
       id={label}
       inputMode="numeric"
       name={name}
-      onChange={onChange}
+      onChange={handleChange}
       pattern="[0-9]*"
       value={value}
       type="text"
